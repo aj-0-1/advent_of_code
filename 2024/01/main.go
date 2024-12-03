0796ef94e7cbf0ecf698063a29b26f50a1d95275
Amirzan Jeyakumar <jeyakumaramirzan@gmail.com>
2024-12-03 01:20:54 +0000

    2024/01: clean up day 1 solutions

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -10,108 +12,86 @@ import (
 )
 
 func main() {
-	l := newListsData()
-	err := l.parseInputFile("input.txt")
-	if err != nil {
-		fmt.Printf("failed to parse input file: %v\n", err)
+	var left, right []int
+	var totalDistance, similarityScore int
+	rightOccurences := make(map[int]int)
+
+	parseLine := func(line string) (int, int, error) {
+		parts := strings.Fields(line)
+		if len(parts) != 2 {
+			return 0, 0, fmt.Errorf("expected 2 numbers, got %d", len(parts))
+		}
+
+		num1, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return 0, 0, err
+		}
+
+		num2, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return 0, 0, err
+		}
+
+		return num1, num2, nil
 	}
 
-	l.calculateTotalDistance()
-	fmt.Printf("Total distance between lists: %d\n", l.TotalDistance)
+	parseInputFile := func(filePath string) error {
+		file, err := os.Open(filePath)
+		if err != nil {
+			return nil
+		}
+		defer file.Close()
 
-	l.calculateSimilarityScore()
-	fmt.Printf("Similarity Score: %d\n", l.SimilarityScore)
-}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			num1, num2, err := parseLine(line)
+			if err != nil {
+				return err
+			}
 
-type ListsData struct {
-	List1           []int
-	List2           []int
-	TotalDistance   int
-	List2Occurences map[int]int
-	SimilarityScore int
-}
+			left = append(left, num1)
+			right = append(right, num2)
+		}
 
-func newListsData() *ListsData {
-	return &ListsData{
-		List1:           make([]int, 0),
-		List2:           make([]int, 0),
-		TotalDistance:   0,
-		List2Occurences: make(map[int]int),
-		SimilarityScore: 0,
-	}
-}
+		if len(left) != len(left) {
+			return fmt.Errorf("left length (%d) does not match right length (%d)", len(left), len(right))
+		}
 
-func parseLine(line string) (int, int, error) {
-	parts := strings.Fields(line)
-	if len(parts) != 2 {
-		return 0, 0, fmt.Errorf("expected 2 numbers, got %d", len(parts))
-	}
-
-	num1, err := strconv.Atoi(parts[0])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	num2, err := strconv.Atoi(parts[1])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return num1, num2, nil
-}
-
-func (l *ListsData) parseInputFile(filePath string) error {
-	file, err := os.Open(filePath)
-	if err != nil {
 		return nil
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		num1, num2, err := parseLine(line)
-		if err != nil {
-			return err
+	calcTotalDistance := func(left, right []int) {
+		sort.Ints(left)
+		sort.Ints(right)
+
+		for i := range len(left) {
+			a := left[i]
+			b := right[i]
+
+			totalDistance += int(math.Abs(float64(a) - float64(b)))
 		}
 
-		l.List1 = append(l.List1, num1)
-		l.List2 = append(l.List2, num2)
+		fmt.Printf("Total Distance: %d\n", totalDistance)
 	}
 
-	if len(l.List1) != len(l.List2) {
-		return fmt.Errorf("list1 length (%d) does not match list2 length (%d)", len(l.List1), len(l.List2))
-	}
-
-	return nil
-}
-
-func (l *ListsData) calculateTotalDistance() {
-	sort.Ints(l.List1)
-	sort.Ints(l.List2)
-
-	for i := range len(l.List1) {
-		a := l.List1[i]
-		b := l.List2[i]
-
-		var diff int
-		if a > b {
-			diff = a - b
-		} else {
-			diff = b - a
+	calcSimilarityScore := func(left, right []int) {
+		for _, num := range right {
+			rightOccurences[num]++
 		}
 
-		l.TotalDistance += diff
-	}
-}
+		for _, num := range left {
+			similarityScore += num * rightOccurences[num]
+		}
 
-func (l *ListsData) calculateSimilarityScore() {
-	for _, num := range l.List2 {
-		l.List2Occurences[num]++
+		fmt.Printf("Similarity Score: %d\n", similarityScore)
 	}
 
-	for _, num := range l.List1 {
-		occurences := l.List2Occurences[num]
-		l.SimilarityScore += num * occurences
+	err := parseInputFile("input.txt")
+	if err != nil {
+		log.Panic(err)
 	}
+
+	calcTotalDistance(left, right)
+	calcSimilarityScore(left, right)
 }
